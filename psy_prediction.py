81494f8635cfe6715c215b8ea8af11c422ca09fc
Armaanjeswani <123262595+Armaanjeswani@users.noreply.github.com>
2024-05-05 13:45:21 +0530

    Update psy_prediction.py

--- psy_prediction.py
+++ psy_prediction.py
@@ -3,7 +3,9 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import LSTM, Dense
+from keras.models import model_from_json
 from keras.models import load_model
+import json
 
 # Load data
 data = pd.read_csv("D:\Psy\psy_prediction\physiomize data1.csv")
@@ -35,12 +37,26 @@ model.compile(optimizer='adam', loss='mean_squared_error')
 # Train the model
 model.fit(X_train, Y_train, epochs=100, batch_size=32)
 
-# Save the trained model
-model.save("psy_prediction.h5")
+# Save the model architecture as JSON
+model_json = model.to_json()
+with open("psy_prediction_architecture.json", "w") as json_file:
+    json_file.write(model_json)
 
+# Save the trained model weights
+model.save("psy_prediction_weights.h5")
 
-model = load_model("psy_prediction.h5")
+# Load the model architecture from JSON
+with open('psy_prediction_architecture.json', 'r') as json_file:
+    loaded_model_json = json_file.read()
 
+# Load the architecture
+loaded_model = model_from_json(loaded_model_json)
+
+# Compile the loaded model with loss and metrics
+loaded_model.compile(optimizer='adam', loss='mean_squared_error')
+
+# Load the trained model weights
+loaded_model.load_weights('psy_prediction_weights.h5')
 
 num_weeks = int(input("Enter the number of weeks: "))
 input_data = []
@@ -48,11 +64,9 @@ for i in range(num_weeks):
     input_value = float(input(f"Enter value for week {i+1}: "))
     input_data.append(input_value)
 
-
 input_array = np.reshape(np.array(input_data), (1, num_weeks, 1))
 
-
-prediction = model.predict(input_array)
+prediction = loaded_model.predict(input_array)
 scaling_factor = 0.8  # Adjust this scaling factor as needed
 noise = np.random.normal(loc=0, scale=0.1, size=prediction.shape)  # Adding noise to the prediction
 predicted_time = int((prediction[0][0] + noise[0][0]) * max_time * scaling_factor)  # Scale the prediction back to the original range with scaling factor
